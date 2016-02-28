dc2299fe5f49f7e163ead78040a516a7f339ed6c
SerhiiStepanovych <hunterla2@gmail.com>
2016-02-28 21:24:27 +0200

    Update GitHub provider parameters

--- MvcOAuthSimple/App_Start/Startup.Auth.cs
+++ MvcOAuthSimple/App_Start/Startup.Auth.cs
@@ -45,9 +45,30 @@ namespace MvcOAuthSimple
             // This is similar to the RememberMe option when you log in.
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
-            app.UseGitHubAuthentication(
-                clientId: "575627de016c7157ab5f",
-                clientSecret: "a0d0e9155715808edb3bf01e0d90654b1da41713");
+            var options = new GitHubAuthenticationOptions
+            {
+                ClientId = "575627de016c7157ab5f",
+                ClientSecret = "a0d0e9155715808edb3bf01e0d90654b1da41713",
+                Provider = new GitHubAuthenticationProvider
+                {
+                    OnAuthenticated = async context =>
+                    {
+                        // Retrieve the OAuth access token to store for subsequent API calls
+                        string accessToken = context.AccessToken;
+
+                        // Retrieve the username
+                        string gitHubUserName = context.UserName;
+
+                        // Retrieve the user's email address
+                        string gitHubEmailAddress = context.Email;
+
+                        // You can even retrieve the full JSON-serialized user
+                        var serializedUser = context.User;
+                    }
+                }
+            };
+            options.Scope.Add("user,repo");
+            app.UseGitHubAuthentication(options);
         }
     }
 }
